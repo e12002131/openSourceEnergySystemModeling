564e838657aa05d44bb2799213cd5bc7003a94d8
e12002131 <e12002131@student.tuwien.ac.at>
2024-04-08 02:51:21 +0200

    ruff reformat

--- example.py
+++ example.py
@@ -8,19 +8,14 @@ synthload = importSynthload()
 load = initializeLoadProfile(synthload)
 
 # add H0, G0, G1, G2 and G3 load
-load = addLoad(synthload, load, 'H0', 8000)
-load = addLoad(synthload, load, 'G0', 8000)
-load = addLoad(synthload, load, 'G1', 8000)
-load = addLoad(synthload, load, 'G2', 8000)
-load = addLoad(synthload, load, 'G3', 8000)
+load = addLoad(synthload, load, "H0", 8000)
+load = addLoad(synthload, load, "G0", 8000)
+load = addLoad(synthload, load, "G1", 8000)
+load = addLoad(synthload, load, "G2", 8000)
+load = addLoad(synthload, load, "G3", 8000)
 
 # pick day 10
 day_of_load = pickDay(load, 10)
 
 # display 'day_of_load'
 display(day_of_load)
-
-
-
-
-
